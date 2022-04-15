95505b9aabf61eec7d8c8c4c6a238f874af88440
Olushola <olushola251@gmail.com>
2022-04-15 15:40:19 +0100

    created react second react app from scratch - webpack

---
+++ app-two-webpack/src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const App = () => {
+  return <div>SOME REACT BAREMETAL</div>;
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
